14c3fb99569034cdedbf8cf39026d3ad7a953fbd
Stephane Epardaud <stef@epardaud.fr>
2017-07-12 16:01:25 +0200

    Added ApiResource

---
+++ src/main/java/org/mygroup/vertxrs/wiki/ApiResource.java
@@ -0,0 +1,166 @@
+package org.mygroup.vertxrs.wiki;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+import org.mygroup.vertxrs.Async;
+
+import com.github.rjeschke.txtmark.Processor;
+
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
+import io.vertx.rxjava.core.http.HttpServerRequest;
+import rx.Single;
+
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/wiki/api")
+public class ApiResource {
+	@Async
+	@GET
+	@Path("pages")
+	public Single<Response> apiRoot(){
+		return SQL.doInConnection(connection -> connection.rxQuery(SQL.SQL_ALL_PAGES_DATA))
+				.map(res -> {
+					JsonObject response = new JsonObject();
+					List<JsonObject> pages = res.getResults()
+							.stream()
+							.map(obj -> new JsonObject()
+									.put("id", obj.getInteger(1))
+									.put("name", obj.getString(0)))
+							.collect(Collectors.toList());
+					response
+					.put("success", true)
+					.put("pages", pages);
+					return Response.ok(response).build();
+				}).onErrorReturn(x -> {
+					// FIXME: turn into custom exception reporter
+					JsonObject response = new JsonObject();
+					response
+					.put("success", false)
+					.put("error", x.getMessage());
+					return Response.serverError().entity(response).build();
+				});
+	}
+
+	@Async
+	@GET
+	@Path("pages/{id}")
+	public Single<Response> apiGetPage(@PathParam("id") String id){
+		return SQL.doInConnection(connection -> connection.rxQueryWithParams(SQL.SQL_GET_PAGE_BY_ID, new JsonArray().add(id)))
+				.map(res -> {
+					JsonObject response = new JsonObject();
+					if (!res.getResults().isEmpty()) {
+						JsonArray row = res.getResults().get(0);
+						JsonObject payload = new JsonObject()
+								.put("name", row.getString(0))
+								.put("id", id)
+								.put("markdown", row.getString(1))
+								.put("html", Processor.process(row.getString(1)));
+						response
+						.put("success", true)
+						.put("page", payload);
+						return Response.ok(response).build();
+					} else {
+						response
+						.put("success", false)
+						.put("error", "There is no page with ID " + id);
+						return Response.status(Status.NOT_FOUND).entity(response).build();
+					}
+				}).onErrorReturn(x -> {
+					// FIXME: turn into custom exception reporter
+					JsonObject response = new JsonObject();
+					response
+					.put("success", false)
+					.put("error", x.getMessage());
+					return Response.serverError().entity(response).build();
+				});
+	}
+
+	@Async
+	@POST
+	@Path("pages")
+	public Single<Response> apiCreatePage(JsonObject page, @Context HttpServerRequest req){
+		// FIXME: use BeanValidation?
+		Response errorResponse = validateJsonPageDocument(req, page, "name", "markdown");
+		if (errorResponse != null) {
+			return Single.just(errorResponse);
+		}
+		JsonArray params = new JsonArray();
+		params.add(page.getString("name")).add(page.getString("markdown"));
+		return SQL.doInConnection(connection -> connection.rxUpdateWithParams(SQL.SQL_CREATE_PAGE, params))
+				.map(res -> Response.status(Status.CREATED).entity(new JsonObject().put("success", true)).build())
+				.onErrorReturn(x -> {
+					// FIXME: turn into custom exception reporter
+					JsonObject response = new JsonObject();
+					response
+					.put("success", false)
+					.put("error", x.getMessage());
+					return Response.serverError().entity(response).build();
+				});
+	}
+
+	private Response validateJsonPageDocument(HttpServerRequest req, JsonObject page, String... expectedKeys) {
+		if (!Arrays.stream(expectedKeys).allMatch(page::containsKey)) {
+			System.err.println("Bad page creation JSON payload: " + page.encodePrettily() + " from " + req.remoteAddress());
+			return Response.status(Status.BAD_REQUEST)
+					.entity(new JsonObject()
+							.put("success", false)
+							.put("error", "Bad request payload"))
+					.build();
+		}
+		return null;
+	}
+
+	@Async
+	@PUT
+	@Path("pages/{id}")
+	public Single<Response> apiUpdatePage(@PathParam("id") String id, JsonObject page,
+			@Context HttpServerRequest req){
+		// FIXME: use BeanValidation?
+		Response errorResponse = validateJsonPageDocument(req, page, "markdown");
+		if (errorResponse != null) {
+			return Single.just(errorResponse);
+		}
+		JsonArray params = new JsonArray();
+		params.add(page.getString("markdown")).add(id);
+		return SQL.doInConnection(connection -> connection.rxUpdateWithParams(SQL.SQL_SAVE_PAGE, params))
+				.map(res -> Response.ok(new JsonObject().put("success", true)).build())
+				.onErrorReturn(x -> {
+					// FIXME: turn into custom exception reporter
+					JsonObject response = new JsonObject();
+					response
+					.put("success", false)
+					.put("error", x.getMessage());
+					return Response.serverError().entity(response).build();
+				});
+	}
+
+	@Async
+	@DELETE
+	@Path("pages/{id}")
+	public Single<Response> apiDeletePage(@PathParam("id") String id){
+		return SQL.doInConnection(connection -> connection.rxUpdateWithParams(SQL.SQL_DELETE_PAGE, new JsonArray().add(id)))
+				.map(res -> Response.ok(new JsonObject().put("success", true)).build())
+				.onErrorReturn(x -> {
+					// FIXME: turn into custom exception reporter
+					JsonObject response = new JsonObject();
+					response
+					.put("success", false)
+					.put("error", x.getMessage());
+					return Response.serverError().entity(response).build();
+				});
+	}
+}
--- src/main/java/org/mygroup/vertxrs/wiki/SQL.java
+++ src/main/java/org/mygroup/vertxrs/wiki/SQL.java
@@ -10,6 +10,7 @@ import rx.functions.Func1;
 public class SQL {
 	static final String SQL_CREATE_PAGES_TABLE = "create table if not exists Pages (Id integer identity primary key, Name varchar(255) unique, Content clob)";
 	static final String SQL_GET_PAGE = "select Id, Content from Pages where Name = ?";
+	static final String SQL_GET_PAGE_BY_ID = "select Name, Content from Pages where Id = ?";
 	static final String SQL_CREATE_PAGE = "insert into Pages values (NULL, ?, ?)";
 	static final String SQL_SAVE_PAGE = "update Pages set Content = ? where Id = ?";
 	static final String SQL_ALL_PAGES = "select Name from Pages";
