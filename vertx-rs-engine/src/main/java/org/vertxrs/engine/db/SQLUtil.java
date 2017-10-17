6be5b69bf9a5fd6705c38f580378dee1716abed8
Stephane Epardaud <stef@epardaud.fr>
2017-10-17 17:06:53 +0200

    Ouch: missing file due to git ignore

---
+++ vertx-rs-engine/src/main/java/org/vertxrs/engine/db/SQLUtil.java
@@ -0,0 +1,23 @@
+package org.vertxrs.engine.db;
+
+import org.vertxrs.engine.core.AppGlobals;
+
+import io.vertx.rxjava.ext.sql.SQLConnection;
+import rx.Single;
+import rx.functions.Func1;
+
+public class SQLUtil {
+	public static Single<SQLConnection> getConnection(){
+		return AppGlobals.get().getDbConnection();
+	}
+	
+	public static <T> Single<T> doInConnection(Func1<? super SQLConnection, ? extends Single<T>> func){
+		Single<SQLConnection> connection = getConnection();
+		return connection.flatMap(conn -> {
+			return func.call(conn).doAfterTerminate(() -> {
+				conn.close();
+			});
+		});
+	}
+
+}
