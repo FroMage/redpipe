cd7bac5a67141bd23d48d746605607f427b179bc
Stephane Epardaud <stef@epardaud.fr>
2018-03-26 16:52:32 +0200

    WIP CDI test

--- redpipe-cdi/src/test/java/net/redpipe/cdi/AppTest.java
+++ redpipe-cdi/src/test/java/net/redpipe/cdi/AppTest.java
@@ -1,38 +1,77 @@
 package net.redpipe.cdi;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import io.vertx.ext.unit.Async;
+import io.vertx.ext.unit.TestContext;
+import io.vertx.ext.unit.junit.VertxUnitRunner;
+import io.vertx.ext.web.client.WebClientOptions;
+import io.vertx.rxjava.ext.web.client.WebClient;
+import io.vertx.rxjava.ext.web.codec.BodyCodec;
+import net.redpipe.engine.core.Server;
+
+@RunWith(VertxUnitRunner.class)
+public class AppTest {
+    
+    private Server server;
+    private WebClient webClient;
+
+    @Before
+    public void before(TestContext context) {
+        Async async = context.async();
+        server = new Server();
+        server.start().subscribe(v -> {
+            System.err.println("Server started");
+            webClient = WebClient.create(server.getVertx(),
+                    new WebClientOptions().setDefaultHost("localhost").setDefaultPort(9000).setIdleTimeout(0));
+            async.complete();
+        }, x -> { 
+            x.printStackTrace();
+            context.fail(x);
+            async.complete();
+        });
+    }
+    
+    @After
+    public void after(TestContext context) {
+        Async async = context.async();
+        server.close(result -> {
+            if(result.failed())
+                context.fail(result.cause());
+            async.complete();
+        });
     }
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
+    @Test
+    public void testCdi(TestContext context) {
+      final ConsoleHandler consoleHandler = new ConsoleHandler();
+      consoleHandler.setLevel(Level.FINEST);
+      consoleHandler.setFormatter(new SimpleFormatter());
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
+      final Logger app = Logger.getLogger("org.jboss.weld.vertx");
+      app.setLevel(Level.FINEST);
+      app.addHandler(consoleHandler);
+
+        Async async = context.async();
+        webClient.get("/test")
+        .as(BodyCodec.string())
+        .rxSend()
+        .subscribe(body -> {
+            context.assertEquals(200, body.statusCode());
+            context.assertEquals("hello", body.body());
+            async.complete();
+        }, x -> { 
+            x.printStackTrace();
+            context.fail(x);
+            async.complete();
+        });
     }
 }
---
+++ redpipe-cdi/src/test/java/net/redpipe/cdi/CdiResource.java
@@ -0,0 +1,33 @@
+package net.redpipe.cdi;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.enterprise.event.Observes;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+
+import org.jboss.weld.vertx.VertxConsumer;
+import org.jboss.weld.vertx.VertxEvent;
+
+import io.vertx.rxjava.core.Vertx;
+import rx.Single;
+
+@ApplicationScoped
+@Path("test")
+public class CdiResource {
+    @GET
+    public Single<Response> test(@Context Vertx vertx) {
+        return vertx.eventBus()
+            .rxSend("echo.address", "mymessage")
+            .map(msg -> {
+                System.err.println("Got reply: "+msg.body());
+                return Response.ok(msg.body()).build();
+            });
+    }
+
+    void echoConsumer(@Observes @VertxConsumer("echo.address") VertxEvent event) {
+        System.err.println("Got event: "+event.getMessageBody());
+        event.setReply(event.getMessageBody());
+    }
+}
